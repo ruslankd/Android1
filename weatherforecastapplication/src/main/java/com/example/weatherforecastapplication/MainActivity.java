905f6f5e328b5d3400e61f979e68c2e79d04b40e
ruslankd <ruslankd@mail.ru>
2020-06-27 12:10:23 +0300

    Lesson 1

--- weatherforecastapplication/src/main/java/com/example/weatherforecastapplication/MainActivity.java
+++ weatherforecastapplication/src/main/java/com/example/weatherforecastapplication/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.weatherforecastapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+
+        String[] cities = new String[]{
+                "Moscow",
+                "St. Petersburg",
+                "Yekaterinburg",
+                "Sochi",
+                "Vladivostok"
+        };
+
+        ArrayAdapter<String> spinnerArrayAdapter = new ArrayAdapter<>(
+                this,R.layout.spinner_item, cities
+        );
+        spinnerArrayAdapter.setDropDownViewResource(R.layout.spinner_item);
+        spinner.setAdapter(spinnerArrayAdapter);
     }
 }
