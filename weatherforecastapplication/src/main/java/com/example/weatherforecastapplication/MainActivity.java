94269a6c8bcc2b99a01a4ac57cbe819dd77cada0
Ruslan <ruslankd@mail.ru>
2020-06-29 10:52:12 +0300

    Lesson 2

--- weatherforecastapplication/src/main/java/com/example/weatherforecastapplication/MainActivity.java
+++ weatherforecastapplication/src/main/java/com/example/weatherforecastapplication/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.TextView;
 
@@ -28,6 +29,11 @@ public class MainActivity extends AppCompatActivity {
 
     public void clickChangeCity(View view) {
         Intent intent = new Intent(this, CitySelectionActivity.class);
+        startActivity(intent);
+    }
+
+    public void clickButtonSettings(View view) {
+        Intent intent = new Intent(this, SettingsActivity.class);
         startActivity(intent);
     }
 }
